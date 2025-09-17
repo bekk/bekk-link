436c3edeeed430f089665d9645111c8645532919
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-09-17 13:43:08 +0200

    Legg til slettefunksjonalitet

---
+++ app/routes/$slug.delete.tsx
@@ -0,0 +1,23 @@
+import type { LoaderFunction } from "@remix-run/server-runtime";
+import { redirect } from "@remix-run/server-runtime";
+import { prisma } from "~/db.server";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const slug = params.slug;
+  if (typeof slug !== "string") {
+    throw new Error("slug needs to be a string");
+  }
+  const match = await prisma.link.findFirst({
+    where: { slug: { equals: slug.toLowerCase() } },
+  });
+
+  if (!match) {
+    throw new Response("Link not found", { status: 404 });
+  }
+
+  prisma.link.delete({
+    where: { id: match.id },
+  });
+
+  return redirect("/");
+};
